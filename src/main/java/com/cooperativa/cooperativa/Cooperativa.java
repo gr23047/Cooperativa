7d8747be8c94684b94154663bde5d3606e05665e
godofredo <gr23047@ues.edu.sv>
2026-05-03 21:35:51 -0600

    prueba de creacion de asiento exitosa

--- src/main/java/com/cooperativa/cooperativa/Cooperativa.java
+++ src/main/java/com/cooperativa/cooperativa/Cooperativa.java
@@ -1,20 +1,28 @@
-
 package com.cooperativa.cooperativa;
 
-import com.cooperativa.cooperativa.control.Controladora;
-import com.cooperativa.cooperativa.control.Cuenta;
+import com.cooperativa.cooperativa.control.Asiento;
+import com.cooperativa.cooperativa.servicio.AsientoServicio;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class Cooperativa {
+
     public static void main(String[] args) {
-//        Controladora control=new Controladora();
-//        Cuenta cuenta=new Cuenta();
-//        cuenta.setCodigo("1001010101");
-//        cuenta.setNombre("Cuenta prueba");
-//        cuenta.setTipo("Gasto");
-//        cuenta.setNaturaleza("Deudora");
-//        cuenta.setClasificacion("Balance");
-//        cuenta.setNivel(4);
-//        cuenta.setActiva(true);
-//        control.crearCuenta(cuenta);
+        LocalDate hoy = LocalDate.now();
+        Date fecha = Date.from(hoy.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        AsientoServicio asientoservicio = new AsientoServicio();
+        Asiento asiento1 = new Asiento();
+        asiento1.setNumero("A001");
+        asiento1.setFecha(fecha);
+        asiento1.setConcepto("Apertura de cooperativa xd");
+        asiento1.setTipo("Apertura");
+//        asiento1.setPeriodo();
+        asiento1.setTotalDebe(1500.00);
+        asiento1.setTotalHaber(1500.00);
+        asiento1.setCreadoEn(fecha);
+        asiento1.setEstado("Registrado");
+        asientoservicio.crearAsiento(asiento1);
+
     }
 }
