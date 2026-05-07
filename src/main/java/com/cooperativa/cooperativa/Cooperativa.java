f7e414b9ac56a11b744647f7cb98898520d2ee77
Godofredo Gomez <gr23047@ues.edu.sv>
2026-05-07 12:24:16 -0600

    Prueba y comentarios de partidas y asientos

--- src/main/java/com/cooperativa/cooperativa/Cooperativa.java
+++ src/main/java/com/cooperativa/cooperativa/Cooperativa.java
@@ -1,7 +1,12 @@
 package com.cooperativa.cooperativa;
 
 import com.cooperativa.cooperativa.control.Asiento;
+import com.cooperativa.cooperativa.control.Cuenta;
+import com.cooperativa.cooperativa.control.Partida;
 import com.cooperativa.cooperativa.servicio.AsientoServicio;
+import com.cooperativa.cooperativa.servicio.CuentaServicio;
+import com.cooperativa.cooperativa.servicio.PartidaServicio;
+
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -9,20 +14,29 @@ import java.util.Date;
 public class Cooperativa {
 
     public static void main(String[] args) {
-        LocalDate hoy = LocalDate.now();
-        Date fecha = Date.from(hoy.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        AsientoServicio asientoservicio = new AsientoServicio();
-        Asiento asiento1 = new Asiento();
-        asiento1.setNumero("A001");
-        asiento1.setFecha(fecha);
-        asiento1.setConcepto("Apertura de cooperativa xd");
-        asiento1.setTipo("Apertura");
-//        asiento1.setPeriodo();
-        asiento1.setTotalDebe(1500.00);
-        asiento1.setTotalHaber(1500.00);
-        asiento1.setCreadoEn(fecha);
-        asiento1.setEstado("Registrado");
-        asientoservicio.crearAsiento(asiento1);
-
+//        LocalDate hoy = LocalDate.now();
+//        Date fecha = Date.from(hoy.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//        AsientoServicio asientoservicio = new AsientoServicio();
+//        PartidaServicio partidaServicio = new PartidaServicio();
+//        CuentaServicio cuentaServicio = new CuentaServicio();
+//        Asiento asiento1 = new Asiento();
+//        Partida partida1 = new Partida();
+//        Cuenta cuenta;
+//        cuenta = cuentaServicio.buscarPorCodigo("1101");
+//        asiento1.setNumero("A002");
+//        asiento1.setFecha(fecha);
+//        asiento1.setConcepto("Dinero en caja");
+//        asiento1.setTipo("Normal");
+//        //asiento1.setPeriodo();
+//        partida1.setDebe(750.00);
+//        partida1.setHaber(750.00);
+//        partida1.setAsiento(asiento1);
+//        partida1.setCuenta(cuenta);
+//        asiento1.setTotalDebe(1500.00);
+//        asiento1.setTotalHaber(1500.00);
+//        asiento1.setCreadoEn(fecha);
+//        asiento1.setEstado("Registrado");
+//        asientoservicio.crearAsiento(asiento1);
+//        partidaServicio.crearPartida(partida1);
     }
 }
