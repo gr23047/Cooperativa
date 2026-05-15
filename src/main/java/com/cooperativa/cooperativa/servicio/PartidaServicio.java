7ac42265e6f31f7d59858034d277a60a38f1efec
Godofredo Gomez <gr23047@ues.edu.sv>
2026-05-15 16:47:24 -0600

    Algunos metodos de periodo y partida

--- src/main/java/com/cooperativa/cooperativa/servicio/PartidaServicio.java
+++ src/main/java/com/cooperativa/cooperativa/servicio/PartidaServicio.java
@@ -2,12 +2,17 @@ package com.cooperativa.cooperativa.servicio;
 
 import com.cooperativa.cooperativa.control.Partida;
 import com.cooperativa.cooperativa.persistencia.PartidaJpaController;
+import java.util.List;
 
 public class PartidaServicio {
     PartidaJpaController partidaJPA=new PartidaJpaController();
     
     public void crearPartida(Partida partida){
         partidaJPA.create(partida);
+    }
+
+    public List<Object[]> getMayorizado(Integer id) {
+        return partidaJPA.getMayorizado(id);
     }
     
 }
--- src/main/java/com/cooperativa/cooperativa/servicio/PeriodoServicio.java
+++ src/main/java/com/cooperativa/cooperativa/servicio/PeriodoServicio.java
@@ -2,6 +2,7 @@ package com.cooperativa.cooperativa.servicio;
 
 import com.cooperativa.cooperativa.control.Periodo;
 import com.cooperativa.cooperativa.persistencia.PeriodoJpaController;
+import java.util.List;
 
 public class PeriodoServicio {
 
@@ -10,4 +11,8 @@ PeriodoJpaController periodoJPA=new PeriodoJpaController();
 public Periodo buscarPeriodo(Integer id){
     return periodoJPA.findPeriodo(id);
 }
+
+    public List<Periodo> traerPeriodos() {
+        return periodoJPA.findPeriodoEntities();
+    }
 }
